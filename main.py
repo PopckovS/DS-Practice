4a0166c2da8946d255159db7dd0d24b183d3c91c
Sergio <popckovM10@yandex.ru>
2022-03-02 09:50:09 +0300

    добавил 2-й урок по matplotlib

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import numpy as np
-
-mass = [
-    185,175,170,169,171,172,175,157,170,172,167,173,168,167,166,
-    167,169,172,177,178,165,161,179,159,164,178,172,170,173,171
-]
-
-values, counts = np.unique(mass, return_counts=True)
-
-print('Все значения = ', values)
-print('Количество раз что они встречаются = ', counts)
-
-index = np.argmax(counts)
-
-print('Самое часто встречающееся число = ', values[index])
-print(f'Число {values[index]} встречается {counts[index]} раз')
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 
+def func_1():
+    # Выбор программы для отображения
+    matplotlib.use('Qt5Agg')
 
+    # Узнать что используется для отображения
+    print('Для рисования используем = ', matplotlib.get_backend())
+
+    # Передаем параметры для Y-оси
+    plt.plot([1, 2, -6, 0, 4])
+
+    # Показать график
+    plt.show()
+
+
+
+
+
+func_1()
 
