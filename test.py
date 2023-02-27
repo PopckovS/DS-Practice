498c866f6293b0b4979bb33c4905fea61b5b6755
PopckovS <popckovM10@yandex.ru>
2023-02-27 23:53:14 +0300

    Добавил уроки numpy

--- test.py
+++ test.py
@@ -38,16 +38,55 @@ def f3():
 def f4():
     arr_m = np.array(
         [
-            [],
-            [],
-            []
+            [5,  10, 15],
+            [20, 25, 30],
+            [35, 40, 45]
         ]
     )
+    print(arr_m)
+    print(arr_m.dtype)
+
+    print('Получаем строки матрицы')
+    print('Получаем 1-ю строку массива arr_m[0] = ', arr_m[0])
+    print('Получаем 2-ю строку массива arr_m[1] = ', arr_m[1])
+
+    print('Получаем элементы строк в матрице')
+    print('Получаем 1-й элемент 1-й строки массива arr_m[1][0] = ', arr_m[1][0])
+    print('Специальный синтаксис доступа arr_m[1,0] = ', arr_m[1,0])
+
+    print('Получаем под-матрицу из матрицы')
+    print('Получаем под-матрицу с помощью срезов arr_m[:2, 0:] = \n', arr_m[:2, 1:])
+
+
+def f5():
+    """Выбор по условию. Condition selection(filtering)"""
+    arr = np.arange(1, 11)
+
+    print(arr)
+
+    new_arr = arr > 4
+    print(new_arr)
+    print(arr[new_arr])
+
+    print(arr[arr > 4])
+
+
+def f6():
+    arr = np.arange(1,11)
+
+    print('Простейшие операции с массивом: ', arr)
+    print(arr+5)
+    print(arr-5)
+    print(arr*2)
+    print(arr/2)
+
 
 
 if __name__ == '__main__':
     # f1()
     # f2()
     # f3()
-    f4()
+    # f4()
+    # f5()
+    f6()
 
